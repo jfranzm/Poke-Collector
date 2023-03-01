b11ca5d4a1ca0c4390c17876241a6753520c6515
Jeffrey Martinez <martinez.jeffreyfranz@gmail.com>
2023-02-28 21:59:42 -0500

    include CRUD fxn

--- pokecollector/main_app/urls.py
+++ pokecollector/main_app/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('pokemon/', views.pokemon_index, name='index'),
-    path('pokemon/<int:pokemon_id>/', views.pokemon_detail, name='detail')
+    path('pokemon/', views.pokemon_index, name='pokemon_index'),
+    path('pokemon/<int:pokemon_id>/', views.pokemon_detail, name='pokemon_detail'),
+    path('pokemon/create/', views.PokeCreate.as_view(), name='pokemon_create'),
+    path('pokemon/<int:pk>/update', views.PokeUpdate.as_view(), name='pokemon_update'),
+    path('pokemon/<int:pk>/delete', views.PokeDelete.as_view(), name='pokemon_delete'),
 ]
--- pokecollector/main_app/views.py
+++ pokecollector/main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Pokemon
 
 from django.http import HttpResponse
@@ -14,6 +15,18 @@ def pokemon_index(request):
     return render(request, 'pokemon/index.html', {'pokemons': pokemons})
 
 def pokemon_detail(request, pokemon_id):
-    pokemon = Pokemon.objects.get(id=pokemon_id)
-    return render(request, 'pokemon/detail.html', {'pokemon': pokemon})
+  pokemon = Pokemon.objects.get(id=pokemon_id)
+  return render(request, 'pokemon/detail.html', { 'pokemon': pokemon })
 
+class PokeCreate(CreateView):
+    model = Pokemon
+    fields = '__all__'
+    success_url = '/pokemon/'
+
+class PokeUpdate(UpdateView):
+    model = Pokemon
+    fields = ['type', 'level', 'stage']
+
+class PokeDelete(DeleteView):
+    model = Pokemon
+    success_url = '/pokemon/'
